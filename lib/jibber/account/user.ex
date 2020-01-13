0f3651571ee3fb22cbcc940b25673e339a813228
Liyu Liu <liyu.liu@access-company.com>
2020-01-13 16:25:47 +0800

    liyu

--- lib/jibber/account/user.ex
+++ lib/jibber/account/user.ex
@@ -1,2 +1,9 @@
 defmodule Jibber.Account.User do
+  use Ecto.Schema
+
+  schema "users" do
+    field :email, :string
+    field :password, :string
+    # has_many :posts, Post
+  end
 end
--- mix.exs
+++ mix.exs
@@ -43,6 +43,7 @@ defmodule Jibber.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
+      {:ex_machina, "~> 2.3", only: :test},
       {:mix_test_watch, "~> 1.0", only: :dev, runtime: false}
     ]
   end
