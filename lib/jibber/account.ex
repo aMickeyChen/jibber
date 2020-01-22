bc9f88801175b902ace32a02d5693e70a1e721e8
aChiweiHuang <chiwei.huang@access-company.com>
2020-01-22 16:22:07 +0800

    mike angry

--- lib/jibber/account.ex
+++ lib/jibber/account.ex
@@ -1,8 +1,27 @@
 defmodule Jibber.Account do
+  use Norm
   import Ecto.Query
   alias Jibber.Repo
   alias Jibber.Account.User
+  
+  def email() do
+    spec(is_binary() and &String.match?(&1, ~r([^@]+@[^\.]+\..+)))
+  end
 
+  def password() do
+    spec(is_binary())
+  end
+
+  def user() do
+    schema = schema(%{
+      user: schema(%{
+        email: email(),
+        password: password(),
+      })
+    })
+  end
+
+  @contract login(email :: email(), password :: password()) :: user() 
   def login(email, password) do
     {:ok, Repo.get_by!(User, email: email, password: password)}
   end
