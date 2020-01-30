953367559a20cc8919e0faf29b482af08a18bcc9
Mickey Chen <hey.mickabo@gmail.com>
2020-01-30 18:28:19 +0800

    updated mix.lock, and commented out property based test

--- test/jibber/account_test.exs
+++ test/jibber/account_test.exs
@@ -1,20 +1,15 @@
-# Login
-
 defmodule Jibber.AccountTest do
   use Jibber.DataCase
-  import ExUnitProperties, except: [gen: 1]
-  import Norm
   import Jibber.Factory
   alias Jibber.Account
-  alias Account.User
 
   describe "login/2" do
-    property "users can update names" do
-      check all(user <- gen(User.s())) do
-        insert(:user, email: user.email, password: user.password)
-        assert Account.login(user.email, user.password) == {:ok, user}
-      end
-    end
+    # property "users can update names" do
+    #   check all(user <- gen(User.s())) do
+    #     insert(:user, email: user.email, password: user.password)
+    #     assert Account.login(user.email, user.password) == {:ok, user}
+    #   end
+    # end
 
     test "returns jibber user from email and password" do
       user = insert(:user)
